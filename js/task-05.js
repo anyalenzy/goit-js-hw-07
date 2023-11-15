fb57a97347b5d60ee30535e71daa76fc75b2962a
anyalenzy <anya.dudnik.91@gmail.com>
2023-11-15 15:50:59 +0200

    done task - 5

--- js/task-05.js
+++ js/task-05.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const btn = document.querySelector(".change-color");
+const hexColor = document.querySelector(".color");
+
+const changeColor = () => {
+  body.style.backgroundColor = getRandomHexColor();
+  hexColor.textContent = getRandomHexColor();
+};
+
+btn.addEventListener("click", changeColor);
